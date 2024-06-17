2d5c1436fd301faf07e1ac7c2a9bcac56f57930a
Parth <surveparth429@gmail.com>
2024-06-17 15:28:28 +0530

    Added hamburger menu with full responsiveness

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,11 @@
 
 
-const hamburger = () => {
-    document.getElementById("ham").style.display = block;
+let ham = document.getElementById("hamMenu");
+
+const goToHam = () => {
+    ham.style.left = 0;
+}
+
+const backToHome = () => {
+    ham.style.left = `100%`
 }
