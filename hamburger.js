042b5f4f536b6dd99f94d47fc33b9e20e424b79c
Parth <surveparth429@gmail.com>
2024-06-17 15:33:17 +0530

    Display adjustment of ham

--- hamburger.js
+++ hamburger.js
@@ -4,8 +4,10 @@ let ham = document.getElementById("hamMenu");
 
 const goToHam = () => {
     ham.style.left = 0;
+    ham.style.display = `flex`;
 }
 
 const backToHome = () => {
-    ham.style.left = `100%`
+    ham.style.left = `100%`;
+    ham.style.display = `hidden`;
 }
