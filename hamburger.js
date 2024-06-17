c89c8a0bc5c77eef0209efcf748e0d7457663f3d
Parth <surveparth429@gmail.com>
2024-06-17 15:34:44 +0530

    Display adjustment

--- hamburger.js
+++ hamburger.js
@@ -3,8 +3,8 @@
 let ham = document.getElementById("hamMenu");
 
 const goToHam = () => {
-    ham.style.left = 0;
     ham.style.display = `flex`;
+    ham.style.left = 0;
 }
 
 const backToHome = () => {
