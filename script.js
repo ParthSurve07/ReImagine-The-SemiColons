4db495970e6e351e78f893e6941b775a2f9506e4
Parth <surveparth429@gmail.com>
2024-06-25 17:45:42 +0530

    Initial setup

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+// For login and cart button
+const login = document.querySelector("#login");
+const cart = document.querySelector("#cart");
+
+login.addEventListener('mouseenter', () => {
+    login.style.color = "black";
+    login.style.backgroundColor = "white";
+});
+
+login.addEventListener('mouseleave', () => {
+    login.style.color = "white";
+    login.style.backgroundColor = "black";
+});
+
+cart.addEventListener('mouseenter', () => {
+    cart.style.color = "black";
+    cart.style.backgroundColor = "white";
+});
+
+cart.addEventListener('mouseleave', () => {
+    cart.style.color = "white";
+    cart.style.backgroundColor = "black";
+});
+
+// Transition for category boxes
+const women = document.querySelector("#women");
+const wText = document.querySelector("#wText");
+
+women.addEventListener('mouseenter', () => {
+    wText.style.opacity = 1;
+    wText.style.scale = "250%";
+});
+
+women.addEventListener('mouseleave', () => {
+    wText.style.opacity = 0;
+    wText.style.scale = "100%";
+});
+
+const men = document.querySelector("#men");
+const mText = document.querySelector("#mText");
+
+men.addEventListener('mouseenter', () => {
+    mText.style.opacity = 1;
+    mText.style.scale = "250%";
+});
+
+men.addEventListener('mouseleave', () => {
+    mText.style.opacity = 0;
+    mText.style.scale = "100%";
+});
+
+const kids = document.querySelector("#kids");
+const kText = document.querySelector("#kText");
+
+kids.addEventListener('mouseenter', () => {
+    kText.style.opacity = 1;
+    kText.style.scale = "250%";
+});
+
+kids.addEventListener('mouseleave', () => {
+    kText.style.opacity = 0;
+    kText.style.scale = "100%";
+});
+
+const beauty = document.querySelector("#beauty");
+const bText = document.querySelector("#bText");
+
+beauty.addEventListener('mouseenter', () => {
+    bText.style.opacity = 1;
+    bText.style.scale = "250%";
+});
+
+beauty.addEventListener('mouseleave', () => {
+    bText.style.opacity = 0;
+    bText.style.scale = "100%";
+});
